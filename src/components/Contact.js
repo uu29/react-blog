8094756051e91cd5f2a7ec48ccb3fce5bf1ba878
jlory <uu5474@gmail.com>
2020-07-27 22:35:42 +0900

    20200727-immutable helper

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import update from 'react-addons-update';
 
 export default class Contact extends Component {
   constructor(props) {
@@ -18,6 +19,10 @@ export default class Contact extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleChange(e) {
@@ -31,6 +36,34 @@ export default class Contact extends Component {
       selectedKey: key,
     });
     console.log(key, 'is selected');
+  }
+
+  handleCreate(contact) {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        $push: [contact],
+      }),
+    });
+  }
+
+  handleRemove() {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        $splice: [[this.state.selectedKey, 1]], //selectedKey부터 1개번째 데이터를 삭제할 것임
+      }),
+      selectedKey: -1,
+    });
+  }
+
+  handleEdit(name, phone) {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        [this.state.selectedKey]: {
+          name: {$set: name},
+          phone: {$set: phone},
+        },
+      }),
+    });
   }
 
   render() {
